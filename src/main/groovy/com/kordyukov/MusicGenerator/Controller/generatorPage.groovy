186257c1c866a9e3e4146891b8554f54223892b0
kordyukov <mrkordyukov@gmail.com>
2022-01-26 12:52:54 +0200

    Changes MusicGenerator

--- src/main/groovy/com/kordyukov/MusicGenerator/Controller/generatorPage.groovy
+++ src/main/groovy/com/kordyukov/MusicGenerator/Controller/generatorPage.groovy
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
