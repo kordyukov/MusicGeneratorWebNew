0f9e7bcdf445ac47cb7b75211b106006df9bde8e
kordyukov <mrkordyukov@gmail.com>
2022-02-11 15:20:10 +0200

    update indexes

--- src/main/java/com/kordyukov/musicgenerator/SimpleHttpServer/SimpleHttpServer.java
+++ src/main/java/com/kordyukov/musicgenerator/SimpleHttpServer/SimpleHttpServer.java
@@ -3,6 +3,9 @@ package com.kordyukov.musicgenerator.SimpleHttpServer;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.kordyukov.musicgenerator.Controller.generatorPage;
 import com.kordyukov.musicgenerator.MusicGeneratorConst;
@@ -21,8 +24,20 @@ public class SimpleHttpServer {
             InetAddress addr = InetAddress.getLocalHost();
             InetSocketAddress socketAddress = new InetSocketAddress(addr, PORT);
             server = HttpServer.create(socketAddress, 3);
+            boolean isBaseDirIdea;
 
-            server.createContext("/", new StaticFileHandler(generatorPage.checkBaseDirTomcat == true ? MusicGeneratorConst.baseDirIdea : MusicGeneratorConst.baseDirIdea));
+            Path path = Paths.get(MusicGeneratorConst.pathTomcat);
+
+            isBaseDirIdea = !Files.exists(path);
+
+            if(isBaseDirIdea){
+                server.createContext("/", new StaticFileHandler(MusicGeneratorConst.baseDirIdea ));
+                System.out.println("SimpleHttpServer baseDirIdea");
+            } else {
+                server.createContext("/", new StaticFileHandler(MusicGeneratorConst.baseDirTomcat ));
+                System.out.println("SimpleHttpServer baseDirTomcat");
+            }
+
 
             server.start();
             System.out.println("server.start()");
